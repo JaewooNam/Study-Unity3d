83a31a8d1b67d608362c7e1b78a75c3560333967
Jaewoo Nam <namssabu12@gmail.com>
2020-01-07 10:44:43 +0900

    Execution Order

--- Unity Intro/Assets/Scripts/MyFIrstScript.cs
+++ Unity Intro/Assets/Scripts/MyFIrstScript.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyFIrstScript : MonoBehaviour
-{
+public class MyFIrstScript : MonoBehaviour {
+    private void Awake() {
+        Debug.Log("Awake Called");
+    }
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        Debug.Log("Start Called");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Debug.LogWarning("Update Called");
+    }
+
+    private void LateUpdate() {
+        Debug.Log("LateUpdate Called");
     }
 }
