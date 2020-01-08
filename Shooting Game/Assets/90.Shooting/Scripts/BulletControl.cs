3d58b300b28106b3ffbd0fb0d017ff9ef778ac9b
Jaewoo Nam <namssabu12@gmail.com>
2020-01-08 17:00:38 +0900

    Add bullet movement

---
+++ Shooting Game/Assets/90.Shooting/Scripts/BulletControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour {
+    // 총알을 위로 보내기 위한 컨트롤러
+    public float BulletSpeed = 100.0f;
+    // 트랜스폼 컴포넌트를 캐싱한다.
+    private Transform myTransform = null;
+    // Start is called before the first frame update
+    void Start() {
+        // 만약 똑같은 컴포넌트가 여러개 있을 경우? -> 가장 상단에 있는 것을 가져온다.
+        myTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3  moveAmount = BulletSpeed * Vector3.up * Time.deltaTime;
+        myTransform.Translate(moveAmount);
+
+        // 화면 밖으로 나가면 사라지게 한다.
+        if (myTransform.position.y > 60.0f) {
+            // 이 스크립트가 지금 붙어있는 게임 오브젝트
+            Destroy(gameObject); // gameObject = BulletPrefab
+        }
+    }
+}
--- Shooting Game/Assets/90.Shooting/Scripts/PlayerControl.cs
+++ Shooting Game/Assets/90.Shooting/Scripts/PlayerControl.cs
@@ -10,15 +10,19 @@ public class PlayerControl : MonoBehaviour
     // 플레이어 게임오브젝트의 트랜스폼 컴포넌트
     private Transform myTransfrom = null;
     // Start is called before the first frame update
-    void Start()
-    {
-        // 이 컴포넌트를 얻어오겠다.
+    void Start() {
+        // 트랜스폼 컴포넌트를 캐싱.
         myTransfrom = GetComponent<Transform>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        // -1 ~ 1 왼쪽 화살키(-1) 오른쪽 화살키 (1)
+        float axis = Input.GetAxis("Horizontal");
         
+        // 매 프레임 당 이 게임 오브젝트가 우리가 원하는 속도와 방향으로 이동하는 양
+        Vector3 moveAmount = axis * Speed * -Vector3.right * Time.deltaTime;
+
+        myTransfrom.Translate(moveAmount);
     }
 }
