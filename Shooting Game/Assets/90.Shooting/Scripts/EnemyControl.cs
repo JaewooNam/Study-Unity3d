e57c765d359fc823dde5d02d2c59d45f185db0ad
Jaewoo Nam <namssabu12@gmail.com>
2020-01-08 17:50:42 +0900

    Create Enemy

---
+++ Shooting Game/Assets/90.Shooting/Scripts/EnemyControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour {
+    public float EnemySpeed = 50.0f;
+    private Transform myTransform = null;
+    // Start is called before the first frame update
+    void Start() {
+        myTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 moveAmount = EnemySpeed * Vector3.back *  Time.deltaTime;
+        myTransform.Translate(moveAmount);
+
+        // 화면 밖으로 나갔다면 위치를 다시 세팅.
+        if (myTransform.position.y < -50.0f) {
+            myTransform.position = new Vector3(Random.Range(
+                -60.0f, 60.0f), 50.0f, 0.0f
+            );
+        }
+    }
+}
