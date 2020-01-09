4646aa7bf1e39edcaafb37276ac830fed7efa982
Jaewoo Nam <namssabu12@gmail.com>
2020-01-09 15:29:51 +0900

    Make Explosion

--- Shooting Game/Assets/90.Shooting/Scripts/EnemyControl.cs
+++ Shooting Game/Assets/90.Shooting/Scripts/EnemyControl.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyControl : MonoBehaviour {
     public float EnemySpeed = 50.0f;
     private Transform myTransform = null;
+    
+    public GameObject Explosion = null;
     // Start is called before the first frame update
     void Start() {
         myTransform = GetComponent<Transform>();
@@ -17,16 +19,25 @@ public class EnemyControl : MonoBehaviour {
 
         // 화면 밖으로 나갔다면 위치를 다시 세팅.
         if (myTransform.position.y < -50.0f) {
-            myTransform.position = new Vector3(Random.Range(
-                -60.0f, 60.0f), 50.0f, 0.0f
-            );
+            InitPosition();
         }
     }
 
+    // 내 위치를 초기화시켜주는 함수
+    void InitPosition() {
+        myTransform.position = new Vector3(Random.Range(
+            -60.0f, 60.0f), 50.0f, 0.0f
+        );
+    }
     // 나의 충돌체 영역에 트리거 설정이 된 충돌체가 부딪히면 발생하는 이벤트 함수
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Bullet") {
             Debug.Log("Bullet Trigger Enter");
+            // 폭발 이펙트 생성
+            Instantiate(Explosion, myTransform.position, Quaternion.identity);
+
+            InitPosition();
+            Destroy(other.gameObject);
         }
     }
 }
