0599ad67de5d6e32cb93ab693aa03d435d9772d6
Jaewoo Nam <namssabu12@gmail.com>
2020-01-14 12:33:14 +0900

    Create Background Texture

---
+++ Shooting Game/Assets/90.Shooting/Scripts/BackgroundControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundControl : MonoBehaviour
+{
+    public float ScrollSpeed = 0.1f;
+    public MeshRenderer MyRenderer = null;
+    // Start is called before the first frame update
+    void Start() {
+        MyRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // meshrenderer의 offset값을 새로 세팅해줍니다. (매 프레임 마다)
+        MyRenderer.material.SetTextureOffset("_MainTex", new Vector2(0.0f, 
+            -Time.time * ScrollSpeed));
+    }
+}
