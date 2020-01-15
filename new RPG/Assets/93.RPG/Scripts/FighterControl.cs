a733ab0fba17b4eaed7ce15e023a90001aebbcbf
Jaewoo Nam <namssabu12@gmail.com>
2020-01-15 16:46:45 +0900

    Character movement

--- new RPG/Assets/93.RPG/Scripts/FighterControl.cs
+++ new RPG/Assets/93.RPG/Scripts/FighterControl.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class FighterControl : MonoBehaviour {
 
     [Header("이동관련 속성")]
+    [Tooltip("기본이동속도")]
     public float MoveSpeed = 2.0f;
+    [Tooltip("달리기속도")]
     public float RunSpeed = 3.5f;
     public float DirectionRotateSpeed = 100.0f;
     public float BodyRotateSpeed = 2.0f; // 몸통의 방향을 변경하기 위한 속도
@@ -19,11 +21,34 @@ public class FighterControl : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        
+        myCharacterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update() {
         
     }
+
+    private void OnAnimatorMove() {
+      
+    }
+
+    void Move() {
+        // 현재 메인카메라 게임 오브젝트의 트랜스폼 컴포넌트를 가져옴
+        Transform CameraTransform = Camera.main.transform;
+        // 실제 카메라가 바라보는 방향이 월드 상에서는 어떤 방향인지 얻어옴
+        Vector3 forward = CameraTransform.TransformDirection(Vector3.forward);
+        forward.y = 0.0f;
+
+        // y축은 무시.
+        Vector3 right = new Vector3(forward.z, 0.0f, -forward.x);
+
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        // 우리가 이동하고자하는 방향
+        Vector3 targetDirection = horizontal * right + vertical * forward;
+    }
+
+    
 }
